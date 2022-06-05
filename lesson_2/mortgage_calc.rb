18845422c7f08fff64fad5f6b7a5d8f6570587c1
Jonathan Hurd <hurd.jonathan@gmail.com>
2022-06-05 17:41:41 -0500

    Reworked the input validation

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -1,7 +1,8 @@
 require "bigdecimal"
 
-# Calculates monthly payments with interest included
 def get_monthly_payments(loan_amount, monthly_interest, loan_duration)
+  return (loan_amount.to_f / loan_duration) if monthly_interest == 0
+
   loan_amount *
     (monthly_interest / (1 - (1 + monthly_interest)**(-loan_duration)))
 end
@@ -11,25 +12,12 @@ def form_num(num)
   format('%.2f', num)
 end
 
-def float?(num)
-  /\d/.match?(num) && /^-?\d*\.?\d*$/.match?(num)
-end
-
-# 
-def integer?(num)
-  /^\d+$/.match?(num)
-end
-
-# Uses integer? and float? methods to check if num is a valid input
-def valid_num?(num)
-  integer?(num) || float?(num)
-end
-
 def get_loan_amount
   loop do
     puts "Please enter the loan amount in dollars"
     amount = gets.chomp
-    return amount.to_i if valid_num?(amount)
+    return amount.to_i if /^[1-9]\d*$/.match?(amount) # Any non-zero int
+    puts "Not a valid number"
   end
 end
 
@@ -37,7 +25,8 @@ def get_loan_duration
   loop do
     puts "Please enter the loan duration in months"
     duration = gets.chomp
-    return duration.to_i if integer?(duration)
+    return duration.to_i if /^[1-9]\d*$/.match?(duration)
+    puts "Not a valid number"
   end
 end
 
@@ -45,7 +34,10 @@ def get_interest_rate
   loop do
     puts "Please enter the Annual Percentage Rate. Example: 10.3 for 10.3%"
     rate = gets.chomp
-    return BigDecimal(rate) if valid_num?(rate)
+    # This regex matches digits followed by optional decimal point and more
+    # optional digits. OR matches decimal point followed by digits.
+    return BigDecimal(rate) if /^((\d+\.?\d*)|(\.\d+))$/.match?(rate)
+    puts "Not a valid number"
   end
 end
 
@@ -129,6 +121,8 @@ def print_amortization(table, alignment)
   end
 end
 
+system("clear")
+
 puts "Welcome to the mortgage calculator!"
 
 loan_amount, loan_duration, interest_rate = get_amount_duration_rate
@@ -139,7 +133,7 @@ loop do # Main loop
 
   puts %{
 
-    Loan amount   > $#{loan_amount}
+    Loan Amount   > $#{loan_amount}
     Interest Rate > #{form_num(interest_rate)}%
     Loan Duration > #{loan_duration} months
 
@@ -150,12 +144,12 @@ loop do # Main loop
     Your total interest payed will be > $#{form_num(total_interest)}
 
     Options:
-      1) Change Loan amount
+      1) Change loan amount
       2) Change loan duration
-      3) Change interesterest rate
+      3) Change interest rate
       4) Restart the calculator
       5) Calculate amortization table
-      6) Exit the Calculator
+      6) Exit the calculator
 
     }
 
@@ -169,6 +163,8 @@ loop do # Main loop
   when "3"
     interest_rate = get_interest_rate
   when "4"
+    system("clear")
+
     loan_amount, loan_duration, interest_rate = get_amount_duration_rate
   when "5"
     alignment = [0, 20, 40, 60]
